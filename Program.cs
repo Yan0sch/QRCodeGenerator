571e2e2650c228b6abf64571f31ee7348d567f37
Yanosch <yan_osch@gmx.de>
2021-05-19 22:48:11 +0200

    Create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace qr_codes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
